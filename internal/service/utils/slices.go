0d431957de8ad44befa6fb88e9d98514a4b021f0
awlsring <mattcanemail@gmail.com>
2023-02-25 16:26:45 -0800

    feat: decode ssh key function

--- internal/service/utils/slices.go
+++ internal/service/utils/slices.go
@@ -1,7 +1,27 @@
 package utils
 
-import "strings"
+import (
+	"net/url"
+	"strings"
+)
 
 func StringLinedToSlice(s string) []string {
 	return strings.Split(s, "\n")
+}
+
+func StringToStringList(stringList string) []string {
+	var l []string
+	for _, str := range strings.Split(stringList, "\n") {
+		l = append(l, str)
+	}
+	return l
+}
+
+func DecodeStringList(s *string) []string {
+	if s == nil {
+		return nil
+	}
+	un := strings.Replace(*s, "%20", "+", -1)
+	decodedKey, _ := url.QueryUnescape(un)
+	return StringToStringList(decodedKey)
 }
